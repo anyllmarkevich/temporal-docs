5db6ceec2194e3274d602d4aa1d64fd578cc624c
Anyll Markevich <anyllmarkevich@gmail.com>
2026-04-30 22:57:41 -0600

    Refactored code structure to be a lot more useful and expandable. It almost all works, but for some reason I'm not quite getting the right output.

--- Rust Parser/temporal_document_analyzer/src/lib.rs
+++ Rust Parser/temporal_document_analyzer/src/lib.rs
@@ -6,8 +6,8 @@ use undoc::docx::DocxParser;
 use unicode_segmentation::{UWordBounds, UnicodeSegmentation};
 use walkdir::WalkDir;
 
-pub fn hash_people(path: &Path) -> HashMap<String, Person> {
-    let mut people_hash: HashMap<String, Person> = HashMap::new();
+pub fn hash_people(path: &Path) -> HashMap<String, PersonHistory> {
+    let mut people_hash: HashMap<String, NewPerson> = HashMap::new();
     WalkDir::new(path)
         .max_depth(3)
         .min_depth(3)
@@ -24,23 +24,34 @@ pub fn hash_people(path: &Path) -> HashMap<String, Person> {
             people_hash
                 .entry(filename.clone())
                 .and_modify(|person| person.add_path(file.path().to_string_lossy().into_owned()))
-                .or_insert(Person::new(
+                .or_insert(NewPerson::build(
                     filename,
                     file.path().to_string_lossy().into_owned(),
                 ));
         });
     people_hash
+        .into_iter()
+        .map(|(k, v)| (k, v.construct_history()))
+        .collect()
 }
 
 #[derive(Debug, Clone)]
-struct File_Instance {
+pub struct File_Instance {
     path: String,
+    time: String,
     text: String,
 }
 impl File_Instance {
     fn build(path: String) -> File_Instance {
         File_Instance {
             path: path.clone(),
+            time: Path::new(&path)
+                .components()
+                .nth_back(1)
+                .expect("Couldn't find folder name.")
+                .as_os_str()
+                .to_string_lossy()
+                .to_string(),
             text: DocxParser::open(path)
                 .expect("Could not open a file.")
                 .parse()
@@ -50,14 +61,35 @@ impl File_Instance {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct EditInstance {
+    time: String,
+    changes: String,
+}
+impl EditInstance {
+    fn new(timeperiod: String, changes: String) -> EditInstance {
+        EditInstance {
+            time: timeperiod,
+            changes: changes,
+        }
+    }
+    pub fn get_edits(&self) -> &String {
+        &self.changes
+    }
+    pub fn get_timeperiod(&self) -> &String {
+        &self.time
+    }
+}
+
 #[derive(Debug)]
-pub struct Person {
+struct NewPerson {
     filename: String,
     content: Vec<File_Instance>,
 }
-impl Person {
-    fn new(filename: String, path: String) -> Person {
-        Person {
+
+impl NewPerson {
+    fn build(filename: String, path: String) -> NewPerson {
+        NewPerson {
             filename: filename,
             content: vec![File_Instance::build(path)],
         }
@@ -66,8 +98,15 @@ impl Person {
         self.content.push(File_Instance::build(path));
         self.content.sort_by_key(|s| s.path.clone());
     }
-    pub fn find_diffs(&self) -> Vec<String> {
-        let mut diffs_vec: Vec<String> = self
+    pub fn construct_history(self) -> PersonHistory {
+        PersonHistory {
+            diffmap: self.find_diffs(),
+            filename: self.filename,
+            content: self.content,
+        }
+    }
+    fn find_diffs(&self) -> Vec<EditInstance> {
+        let mut diffs_vec: Vec<EditInstance> = self
             .content
             .iter()
             .map(|instance| instance.text.clone())
@@ -90,8 +129,36 @@ impl Person {
                 .collect::<Vec<String>>()
                 .join("")
             })
+            .zip(
+                self.content
+                    .iter()
+                    .map(|x| x.time.clone())
+                    .next()
+                    .into_iter()
+                    .collect::<Vec<String>>(),
+            )
+            .map(|(diffs, time)| EditInstance::new(time, diffs))
             .collect();
-        diffs_vec.insert(0, self.content[0].clone().text);
+        diffs_vec.insert(
+            0,
+            EditInstance::new(self.content[0].time.clone(), self.content[0].text.clone()),
+        );
         diffs_vec
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PersonHistory {
+    diffmap: Vec<EditInstance>,
+    filename: String,
+    content: Vec<File_Instance>,
+}
+impl PersonHistory {
+    pub fn print_history(&self) {
+        println!("———————————————————————————");
+        println!("Printing hisotry from filenames \"{}\".", self.filename);
+        self.diffmap
+            .iter()
+            .for_each(|x| println!("For time {}, \"{}\"", x.get_timeperiod(), x.get_edits()));
     }
 }
--- Rust Parser/temporal_document_analyzer/src/main.rs
+++ Rust Parser/temporal_document_analyzer/src/main.rs
@@ -5,12 +5,7 @@ use walkdir;
 
 fn main() {
     let dir_path = Path::new("/Users/anyll/Documents/My files/  Topics/Work/Work Files/CU Boulder/Andy Martin/Evolutionary Biology/Refined Notebook Code/Examples/Example Folder Structure 1/");
-    //println!("{:?}", hash_people(dir_path))
-    println!(
-        "{:?}",
-        hash_people(dir_path)
-            .values()
-            .map(|x| x.find_diffs())
-            .collect::<Vec<Vec<String>>>()
-    )
+    hash_people(dir_path)
+        .iter()
+        .for_each(|(_, x)| x.print_history());
 }
