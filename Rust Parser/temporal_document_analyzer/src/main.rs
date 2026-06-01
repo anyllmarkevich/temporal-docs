492d4f215858a7aeb16b12dd606853b2c4dcb245
Anyll Markevich <anyllmarkevich@gmail.com>
2026-05-31 18:03:21 -0600

    User input now works correctly via Cli interface!

--- Rust Parser/temporal_document_analyzer/src/main.rs
+++ Rust Parser/temporal_document_analyzer/src/main.rs
@@ -1,11 +1,29 @@
-use std::path::Path;
+use clap::Parser;
+use std::path::{Path, PathBuf};
 use temporal_document_analyzer::*;
 
 fn main() {
-    let dir_path = Path::new("/Users/anyll/Documents/My files/  Topics/Work/Work Files/CU Boulder/Andy Martin/Evolutionary Biology/Refined Notebook Code/Examples/Example Folder Structure 1/");
-    let save_path = Path::new("/Users/anyll/Documents/My files/  Topics/Work/Work Files/CU Boulder/Andy Martin/Evolutionary Biology/Refined Notebook Code/Outputs/Output Tests Generation 1/Test 1/");
-    let database = DatabaseHistory::build(dir_path);
+    let args = Cli::parse();
+    println!(
+        "Seeking data at: {:?}\nOutputting data to: {:?}",
+        args.input_path, args.output_path
+    );
+
+    //let dir_path = Path::new("/Users/anyll/Documents/My files/  Topics/Work/Work Files/CU Boulder/Andy Martin/Evolutionary Biology/Refined Notebook Code/Examples/Example Folder Structure 1/");
+    //let save_path = Path::new("/Users/anyll/Documents/My files/  Topics/Work/Work Files/CU Boulder/Andy Martin/Evolutionary Biology/Refined Notebook Code/Outputs/Output Tests Generation 1/Test 1/");
+    //let database = DatabaseHistory::build(dir_path);
+    //database.print_changelist();
+    //database.save(save_path);
+
+    let database = DatabaseHistory::build(&args.input_path);
     database.print_changelist();
-    database.save(save_path);
+    database.save(&args.output_path);
+
     //println!("{:?}", hash_people(dir_path))
+}
+
+#[derive(Parser)]
+struct Cli {
+    input_path: PathBuf,
+    output_path: PathBuf,
 }
