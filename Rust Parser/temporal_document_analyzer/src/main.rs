4c202d94fd75cf083fa16a6e1d3c656a7475d2a6
Anyll Markevich <anyllmarkevich@gmail.com>
2026-05-31 18:04:54 -0600

    Removed uncessary comments.

--- Rust Parser/temporal_document_analyzer/src/main.rs
+++ Rust Parser/temporal_document_analyzer/src/main.rs
@@ -8,18 +8,9 @@ fn main() {
         "Seeking data at: {:?}\nOutputting data to: {:?}",
         args.input_path, args.output_path
     );
-
-    //let dir_path = Path::new("/Users/anyll/Documents/My files/  Topics/Work/Work Files/CU Boulder/Andy Martin/Evolutionary Biology/Refined Notebook Code/Examples/Example Folder Structure 1/");
-    //let save_path = Path::new("/Users/anyll/Documents/My files/  Topics/Work/Work Files/CU Boulder/Andy Martin/Evolutionary Biology/Refined Notebook Code/Outputs/Output Tests Generation 1/Test 1/");
-    //let database = DatabaseHistory::build(dir_path);
-    //database.print_changelist();
-    //database.save(save_path);
-
     let database = DatabaseHistory::build(&args.input_path);
     database.print_changelist();
     database.save(&args.output_path);
-
-    //println!("{:?}", hash_people(dir_path))
 }
 
 #[derive(Parser)]
